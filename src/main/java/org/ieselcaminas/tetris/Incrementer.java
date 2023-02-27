bb8ba14c00c5b5409595a73a6c835e932048bf3f
alu10701951 <alugar1951@ieselcaminas.org>
2023-02-27 16:04:10 +0100

    Initial commit

---
+++ src/main/java/org/ieselcaminas/tetris/Incrementer.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.ieselcaminas.tetris;
+
+/**
+ *
+ * @author alu10701951
+ */
+public interface Incrementer {
+    public void incrementScore(int increment);
+       
+}
